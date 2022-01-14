418888f2efbeb6e4dd98bced1f9074ffadc118b7
Alexander Morozov <9192541+potatoru@users.noreply.github.com>
2022-01-14 22:48:01 +1100

    Update

--- webpack.mix.js
+++ webpack.mix.js
@@ -36,7 +36,7 @@ mix.js(src + '/js/app.js', 'js').extract().vue().polyfill({
 mix.version().after(() => {
 
   // Compile EJS template
-  const template = fs.readFileSync(src + '/public/index.ejs', 'utf-8');
+  const template = fs.readFileSync(src + '/index.ejs', 'utf-8');
   const html = ejs.render(template, {mix: require('./build/mix-manifest.json')});
   fs.writeFileSync('build/index.html', html);
   fs.unlinkSync('build/mix-manifest.json');
@@ -46,7 +46,7 @@ mix.version().after(() => {
     from: [
       path.resolve(__dirname, 'build'),
       path.resolve(__dirname, 'app/common/public'),
-      path.resolve(__dirname, src + '/public') + '/!(*.ejs)'
+      path.resolve(__dirname, src + '/public')
     ],
     to: new (require('laravel-mix/src/File'))(process.env.BUILD_PATH)
   }).run();

a78222ee50b5e5faf94308dee143a2404ac3feec
Alexander Morozov <9192541+potatoru@users.noreply.github.com>
2022-01-27 14:27:27 +1100

    Small cleanup

--- app/mobile/js/bridge/ios-bridge.js
+++ app/mobile/js/bridge/ios-bridge.js
@@ -1,4 +1,4 @@
-import * as uuid from 'uuid';
+import * as uuid from 'uuid'
 
 const CallbackKey = 'ios-callback';
 
@@ -7,52 +7,51 @@ const CallbackKey = 'ios-callback';
  * результата выполнения функции
  */
 export const iOSBridge = {
-    getStatus: () => sendMessage('getStatus'),
-    requestUiUpdate:() => sendMessage('requestUiUpdate'),
-    audioPlay:() => sendMessage('audioPlay'),
-    audioStop:() => sendMessage('audioStop'),
-    setSleepTimer:(time) => sendMessage('setSleepTimer', [`${time}`]),
-    getAuthToken:() => sendMessage('getAuthToken'),
-    setAuthToken:(token) => sendMessage('setAuthToken', [token]),
-    setLastfmToken:(token, username) => sendMessage('setLastfmToken', [token, username]),
-    getLastfmUsername:() => sendMessage('getLastfmUsername'),
-    getLastfmStatus:() => sendMessage('getLastfmStatus'),
-    getAudioQuality:() => sendMessage('getAudioQuality'),
-    setAudioQuality:(lowQuality) => sendMessage('setAudioQuality', [`${lowQuality}`]),
-    toggleFullscreen:() => sendMessage('toggleFullscreen'),
-    openDrawer:() => sendMessage('openDrawer'),
-    showToast:(msg) => sendMessage('showToast', [msg]),
-    getUserAgent:() => sendMessage('getUserAgent'),
-    getAppVersion:() => sendMessage('getAppVersion'),
-    setBackground:(background) => sendMessage('setBackground', [background]),
-    setReaction: (score) => sendMessage('setReaction', [`${score}`]),
-    getReaction:() => sendMessage('getReaction'),
+  getStatus: () => sendMessage('getStatus'),
+  requestUiUpdate:() => sendMessage('requestUiUpdate'),
+  audioPlay:() => sendMessage('audioPlay'),
+  audioStop:() => sendMessage('audioStop'),
+  setSleepTimer:(time) => sendMessage('setSleepTimer', [`${time}`]),
+  getAuthToken:() => sendMessage('getAuthToken'),
+  setAuthToken:(token) => sendMessage('setAuthToken', [token]),
+  setLastfmToken:(token, username) => sendMessage('setLastfmToken', [token, username]),
+  getLastfmUsername:() => sendMessage('getLastfmUsername'),
+  getLastfmStatus:() => sendMessage('getLastfmStatus'),
+  getAudioQuality:() => sendMessage('getAudioQuality'),
+  setAudioQuality:(lowQuality) => sendMessage('setAudioQuality', [`${lowQuality}`]),
+  toggleFullscreen:() => sendMessage('toggleFullscreen'),
+  openDrawer:() => sendMessage('openDrawer'),
+  showToast:(msg) => sendMessage('showToast', [msg]),
+  getUserAgent:() => sendMessage('getUserAgent'),
+  getAppVersion:() => sendMessage('getAppVersion'),
+  setBackground:(background) => sendMessage('setBackground', [background]),
+  setReaction: (score) => sendMessage('setReaction', [`${score}`]),
+  getReaction:() => sendMessage('getReaction'),
 }
 
 function sendMessage(name, args) {
 
-    if (window.webkit && window.webkit.messageHandlers && window.webkit.messageHandlers.plaza) {
-        const callbackId = uuid.v4();
+  if (window.webkit && window.webkit.messageHandlers && window.webkit.messageHandlers.plaza) {
+    const callbackId = uuid.v4();
 
-        window.webkit.messageHandlers.plaza.postMessage({
-            name, args: args || [], callbackId
-        });
+    window.webkit.messageHandlers.plaza.postMessage({
+      name, args: args || [], callbackId
+    });
 
-        if (!window[CallbackKey]) {
-            window[CallbackKey] = {};
-        }
-
-        const result = new Promise(function(resolve, reject) {
-            window[CallbackKey][callbackId] = (result, errorMessage) => {
-                if (errorMessage) {
-                    reject(new Error(errorMessage))
-                } else {
-                    resolve(result);
-                }
-            }
-         });
-        return result;
-    } else {
-        console.error(`Unable to send message to iOS. Message handler not found`);
+    if (!window[CallbackKey]) {
+      window[CallbackKey] = {};
     }
+
+    return new Promise(function (resolve, reject) {
+      window[CallbackKey][callbackId] = (result, errorMessage) => {
+        if (errorMessage) {
+          reject(new Error(errorMessage))
+        } else {
+          resolve(result);
+        }
+      }
+    });
+  } else {
+    console.error(`Unable to send message to iOS. Message handler not found`);
+  }
 }

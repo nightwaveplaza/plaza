dca9d4b1317cf4e9c0f42b09e91224cb82b09e66
Alexander Morozov <9192541+potatoru@users.noreply.github.com>
2024-03-13 21:52:21 +1100

    Fix mobile player

--- src/mobile/js/windows/WindowPlayer.vue
+++ src/mobile/js/windows/WindowPlayer.vue
@@ -1,5 +1,5 @@
 <template>
-  <win-window name="player" title="Nightwave Plaza" :width="400">
+  <win-window name="player" title="Nightwave Plaza" :width="400" v-show="loaded">
 
     <!-- Minimize button -->
     <template v-slot:header>
@@ -24,11 +24,16 @@
 </template>
 
 <script setup>
-import { computed } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { useStore } from 'vuex'
 import { Native } from '@mobile/js/bridge/native'
+import windowsComposable from '@common/js/composables/windowsComposable'
 
 const store = useStore()
+const loaded = ref(false)
+const win = ref('win')
+
+const { closeWindow } = windowsComposable()
 
 // Reactive data
 const listeners = computed(() => store.getters['player/listeners'])
@@ -41,4 +46,14 @@ function minimize () {
 function toggleFullscreen () {
   Native.toggleFullscreen()
 }
+
+onMounted(() => {
+  store.subscribe((mutation) => {
+    if (mutation.type === 'player/currentSong' && loaded.value === false) {
+      closeWindow('loading')
+      loaded.value = true
+      win.value.pullUp()
+    }
+  })
+})
 </script>

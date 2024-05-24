39b9c20cc824d84a89afd8caf69703fe0c006afc
Alexander <9192541+potatoru@users.noreply.github.com>
2024-05-24 11:22:46 +1100

    Added translation (German)

--- src/common/js/windows/WindowSettingsBackground.vue
+++ src/common/js/windows/WindowSettingsBackground.vue
@@ -97,7 +97,7 @@
                           :value="item[0]"
                           :selected="settingsStore.taskbarPosition === item[0]"
                   >
-                    {{ item[0] }}
+                    {{ item[1] }}
                   </option>
                 </select>
               </div>
--- src/locales/_locales.ts
+++ src/locales/_locales.ts
@@ -1,6 +1,7 @@
 import en from '@locales/en.json'
 import es from '@locales/es.json'
 import ru from '@locales/ru.json'
+import de from '@locales/de.json'
 
 interface JSONValue {
   [key: string]: string | JSONValue
@@ -11,6 +12,7 @@ interface LocaleArray {
 }
 
 export default {
+  'de': de,
   'en': en,
   'es': es,
   'ru': ru

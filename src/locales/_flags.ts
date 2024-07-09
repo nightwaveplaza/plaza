955a1efe6b018c3823b148f24691a94d3c78ae5b
Alexander <9192541+potatoru@users.noreply.github.com>
2024-07-09 18:14:38 +1100

    Redesign WindowSettingsLanguage.vue

---
+++ src/locales/_flags.ts
@@ -0,0 +1,11 @@
+import { BR, DE, US, RU, ES } from 'country-flag-icons/string/3x2'
+
+export default {
+  'en': US,
+  'de': DE,
+  'pt-BR': BR,
+  'ru': RU,
+  'es': ES,
+} as {
+  [key: string]: string
+}

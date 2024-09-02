cd4e14883419e02ac8847adb21ef88486968a1d9
Alexander <9192541+potatoru@users.noreply.github.com>
2024-09-02 19:49:32 +1100

    Revert "Test: howler.js"
    This reverts commit b4494b91ff2e55d77f0437a6b772ca5832c9d57d.

--- src/app/components/basic/WinPlayer.vue
+++ src/app/components/basic/WinPlayer.vue
@@ -4,6 +4,7 @@
       <div class="cover simple-border noselect">
         <img :src="artwork" alt="artwork" @click="showSongInfo">
       </div>
+      <audio ref="audio" crossorigin="anonymous" preload="auto" @loadedmetadata="audioCanPlay" />
     </div>
 
     <div class="col-12 col-sm">
@@ -80,7 +81,6 @@ import type WinPlayerTime from '@app/components/basic/WinPlayerTime.vue'
 import { useSettingsStore } from '@app/stores/settingsStore'
 import { usePlayerPlaybackStore } from '@app/stores/playerPlaybackStore.ts'
 import { PlayerState } from '@app/types/types.ts'
-import { Howl } from 'howler'
 
 const { t } = useI18n()
 const { startVisual, stopVisual } = useVisual()
@@ -90,6 +90,7 @@ const windowsStore = useWindowsStore()
 const playerSongStore = usePlayerSongStore()
 const playerPlaybackStore = usePlayerPlaybackStore()
 
+const audio = ref<InstanceType<typeof HTMLAudioElement>>()
 const time = ref<InstanceType<typeof WinPlayerTime>>()
 const canvas = ref<InstanceType<typeof HTMLCanvasElement>>()
 
@@ -114,7 +115,6 @@ const timerColor = computed(() => playerPlaybackStore.sleepTime !== 0 ? '#3455DB
 // Non-reactive
 let offline = false
 let volume = 100
-let sound: Howl|null = null
 
 function updateSong (): void {
   if (offline && playerPlaybackStore.state === PlayerState.PLAYING) {
@@ -140,64 +140,40 @@ function play (): void {
 }
 
 function startPlay (): void {
-  let src = 'https://radio.plaza.one/mp3'
+  const noCacheStr = 'nocache=' + Date.now()
+  // audio.value!.type = 'audio/mpeg' TODO: why
   if (settingsStore.lowQuality) {
-    src = 'https://radio.plaza.one/mp3_low'
+    audio.value!.src = 'https://radio.plaza.one/mp3_low?' + noCacheStr
+  } else {
+    audio.value!.src = 'https://radio.plaza.one/mp3?' + noCacheStr
   }
 
-  // Check if sound exists, but not playing (paused)
-  if (sound != null && !sound.playing()) {
-    sound.stop().play()
-    return
-  }
-
-  sound = new Howl({
-    src,
-    html5: true,
-    volume,
-    format: ['mp3']
-  })
-
-  sound.on('play', () => {
-    // @ts-ignore
-    if (sound._sounds && sound._sounds[0]._node) {
-      // @ts-ignore
-      const audioNode = sound._sounds[0]._node
-      startVisual(audioNode, canvas.value!)
-      //audioNode.crossOrigin = "use-credentials";
-      updateMediaSession()
-    }
-
-    playerPlaybackStore.state = PlayerState.PLAYING
-  })
-
-  sound.on('pause', () => {
-    stopVisual()
-    playerPlaybackStore.state = PlayerState.IDLE
-  })
-
-  sound.on('seek', () => {
-    console.log(sound?.duration())
-  })
-
-  sound.play()
+  audio.value!.load()
+  audio.value!.volume = volume
 
   document.title = `${playerSongStore.artist} - ${playerSongStore.title}`
 }
 
-function pausePlay (): void {
-  sound?.pause()
+function audioCanPlay (): void {
+  if (playerPlaybackStore.state === PlayerState.LOADING) {
+    playerPlaybackStore.state = PlayerState.PLAYING
+
+    audio.value!.play().then(() => {
+      startVisual(audio.value!, canvas.value!)
+      updateMediaSession()
+      setMediaSessionState('playing')
+    }).catch(e => console.log(e))
+  }
 }
 
 function stopPlay (): void {
   stopVisual()
-  sound!.off('play')
-  sound!.off('pause')
-  sound!.unload()
-  sound = null
+  audio.value!.pause()
+  audio.value!.currentTime = 0
 
   playerPlaybackStore.state = PlayerState.IDLE
   playerPlaybackStore.sleepTime = 0
+  setMediaSessionState('paused')
   document.title = 'Nightwave Plaza - Online Vaporwave Radio'
 }
 
@@ -209,8 +185,7 @@ function setVolume (volume: number): void {
 function updateVolume (newVolume: number): void {
   volume = newVolume / 100
   if (playerPlaybackStore.state === PlayerState.PLAYING) {
-    sound?.volume(volume)
-    //audio.value!.volume = volume
+    audio.value!.volume = volume
   }
 }
 
@@ -260,8 +235,25 @@ function updateMediaSession (): void {
 
 function setMediaSessionActions (): void {
   if ('mediaSession' in navigator) {
-    navigator.mediaSession.setActionHandler('play', () => startPlay());
-    navigator.mediaSession.setActionHandler('pause', () => pausePlay());
+    const actionHandlers = [
+      ['play', play],
+      ['pause', play],
+      ['stop', play],
+    ]
+
+    for (const [action, handler] of actionHandlers) {
+      try {
+        navigator.mediaSession.setActionHandler(action as MediaSessionAction, handler as MediaSessionActionHandler)
+      } catch (error) {
+        console.log(`The media session action "${action}" is not supported yet.`)
+      }
+    }
+  }
+}
+
+function setMediaSessionState (state: string): void {
+  if ('mediaSession' in navigator) {
+    navigator.mediaSession.playbackState = state as MediaSessionPlaybackState
   }
 }
 </script>

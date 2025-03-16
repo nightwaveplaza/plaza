ff891515937a7dda02f466fae0ea1ab51da7f082
Alexander <9192541+potatoru@users.noreply.github.com>
2025-03-16 12:26:05 +1100

    Move taskbar and style to App.vue

--- src/app/views/App.vue
+++ src/app/views/App.vue
@@ -1,15 +1,24 @@
 <template>
-  <router-view />
+  <div class="app-desktop" :class="settingsStore.themeName" :style="{backgroundImage, backgroundColor}">
+    <router-view />
+
+    <win-taskbar />
+    <win-status-bar />
+  </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted, watch } from 'vue'
+import { computed, onMounted, watch } from 'vue'
 
 import { useSettingsStore } from '@app/stores/settingsStore'
 import { useI18n } from 'vue-i18n'
 
 const settingsStore = useSettingsStore()
 const i18n = useI18n()
+
+// Appearance
+const backgroundImage = computed(() => settingsStore.backgroundSrc)
+const backgroundColor = computed(() => settingsStore.background.color)
 
 watch(() => settingsStore.language, () => {
   i18n.locale.value = settingsStore.language
--- src/app/views/Index.vue
+++ src/app/views/Index.vue
@@ -1,14 +1,9 @@
 <template>
-  <div class="app-desktop" :class="settingsStore.themeName" :style="{backgroundImage, backgroundColor}">
-    <component :is="window.component" v-for="window in openedWindows" :key="window.name" :name="window.name" />
-
-    <win-taskbar />
-    <win-status-bar />
-  </div>
+  <component :is="window.component" v-for="window in openedWindows" :key="window.name" :name="window.name" />
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted } from 'vue'
+import { onMounted } from 'vue'
 import { useSettingsStore } from '@app/stores/settingsStore'
 import { useUserAuthStore } from '@app/stores/userAuthStore'
 import { useWindows } from '@app/composables/useWindows.ts'
@@ -16,10 +11,6 @@ import { useWindows } from '@app/composables/useWindows.ts'
 const settingsStore = useSettingsStore()
 const userAuthStore = useUserAuthStore()
 const { openWindow, openedWindows, WinType } = useWindows()
-
-// Appearance
-const backgroundImage = computed(() => settingsStore.backgroundSrc)
-const backgroundColor = computed(() => settingsStore.background.color)
 
 onMounted(() => {
   openWindow(WinType.LOADING)

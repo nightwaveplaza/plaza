1d22d6e74aa7f7ca4a547b8c9b6655012eb1238f
Alexander <9192541+potatoru@users.noreply.github.com>
2025-03-24 16:51:50 +1100

    Added :disable property to WinPagination.vue

--- src/app/components/basic/WinPagination.vue
+++ src/app/components/basic/WinPagination.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="row no-gutters pagination">
     <div v-if="page > 1" class="col-4 pr-1">
-      <win-button class="d-block" @click="nextPage(-1)">
+      <win-button class="d-block" @click="nextPage(-1)" :disabled="props.disabled">
         <i class="i icon-left-hand" />
       </win-button>
     </div>
     <div class="col-4">
-      <input ref="pageInput" v-model.number="page" type="number" class="d-block" @keydown="useNumberOnly">
+      <input ref="pageInput" v-model.number="page" type="number" class="d-block" @keydown="useNumberOnly" :readonly="props.disabled">
     </div>
     <div v-if="pages > 1 && page < pages" class="col-4 pl-1">
-      <win-button class="d-block" @click="nextPage(1)">
+      <win-button class="d-block" @click="nextPage(1)" :disabled="props.disabled">
         <i class="i icon-right-hand" />
       </win-button>
     </div>
@@ -20,9 +20,13 @@
 import { ref, watch } from 'vue'
 import { useNumberOnly } from '@app/composables/useNumberOnly.ts'
 
-const props = defineProps<{
-  pages: number
-}>()
+const props =withDefaults(defineProps<{
+  pages: number,
+  disabled: boolean
+}>(), {
+  pages: 1,
+  disabled: false
+})
 
 const emit = defineEmits(['change'])
 

1e849360bef67b4772111f9036ebb4e4ce65b7b1
Alexander <9192541+potatoru@users.noreply.github.com>
2026-01-19 19:45:05 +1100

    Delay theme color applying

--- src/app/composables/useThemeColor.ts
+++ src/app/composables/useThemeColor.ts
@@ -25,5 +25,11 @@ export function useThemeColor(): void {
   }
 
   watch(() => theme.value, () => setThemeColor())
-  onMounted(() => setThemeColor())
+  onMounted(() => {
+    if (isMobile()) {
+      setTimeout(() => setThemeColor(), 1000)
+    } else {
+      setThemeColor()
+    }
+  })
 }

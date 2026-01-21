0fd565c689b13ff81268e353ccbda9e70e011fca
Alexander <9192541+potatoru@users.noreply.github.com>
2026-01-21 19:55:40 +1100

    Fix binding in WinCaptcha

--- src/app/components/extra/WinCaptcha.vue
+++ src/app/components/extra/WinCaptcha.vue
@@ -5,17 +5,16 @@
         Security check...
       </div>
       <div class="col">
-        <win-loading-bar v-if="response === ''" :height="20" />
+        <win-loading-bar v-if="model === ''" :height="20" />
         <img v-else src="@app/assets/img/check-0.png" style="height: 20px" class="float-end"  alt="ok"/>
       </div>
     </div>
     <vue-turnstile
         v-show="needsInteractive"
-        v-model="response"
+        v-model="model"
         :site-key="turnstileKey"
         size="flexible"
         appearance="interaction-only"
-        :on-update:model-value="onUpdate"
         class="mt-3 lh-1"
         @before-interactive="onBeforeInteractive"
         @error="onError"
@@ -29,17 +28,12 @@ import { ref } from 'vue'
 
 const turnstileKey = import.meta.env.VITE_TURNSTILE_KEY
 
-const response = ref('')
-const model = defineModel<string>()
+const model = defineModel<string>({ default: '' })
 
 const needsInteractive = ref(false)
 
 function onError(error: number) {
   console.error(error)
-}
-
-function onUpdate() {
-  model.value = response.value
 }
 
 function onBeforeInteractive() {

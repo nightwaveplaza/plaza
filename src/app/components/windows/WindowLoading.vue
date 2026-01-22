0d4f4a49c0693cd22b4bb4ffc54a45cbbdfbc5b7
Alexander <9192541+potatoru@users.noreply.github.com>
2026-01-22 11:02:40 +1100

    Use WinLoadingBar component in WindowLoading window

--- src/app/components/windows/WindowLoading.vue
+++ src/app/components/windows/WindowLoading.vue
@@ -1,92 +1,14 @@
 <template>
   <div class="p-3">
-    <p class="text-center">
+    <p class="text-center mb-2">
       <strong>{{ t('loading') }}</strong>
     </p>
-    <div ref="bar" class="text-field progress-bar">
-      <div ref="progress" :style="style" class="progress">
-        <div />
-        <div />
-        <div />
-      </div>
-    </div>
+    <win-loading-bar />
   </div>
 </template>
 
 <script setup lang="ts">
-import { onBeforeUnmount, onMounted, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 
 const { t } = useI18n()
-
-const style = ref({
-  transform: `translate(0px, 0px)`,
-})
-
-const bar = ref<HTMLDivElement | null>(null)
-const progress = ref<HTMLDivElement | null>(null)
-
-// Non-reactive
-let movedAt = 0
-let direction = 3
-let left = 0
-let loading = true
-
-function move (): void {
-  if (!loading) {
-    return
-  }
-
-  const now = Date.now()
-  if (now - movedAt > 30 && bar.value) {
-    if (left > bar.value!.offsetWidth - progress.value!.offsetWidth - 6 || left < 0) {
-      direction *= -1
-    }
-
-    left += direction
-    style.value.transform = `translate(${left}px, 0px)`
-    movedAt = now
-  }
-
-  requestAnimationFrame(move)
-}
-
-onMounted(() => {
-  move()
-})
-
-onBeforeUnmount(() => {
-  loading = false
-})
 </script>
-
-<style lang="scss">
-#window-loading {
-  .progress-bar {
-    position: relative;
-    margin: 12px 5px 0 5px;
-    height: 20px;
-    overflow: hidden;
-    border: 1px solid #808080;
-    border-right-color: #dfdfdf;
-    border-bottom-color: #dfdfdf;
-    padding: 0 6px;
-    min-height: 14px;
-  }
-
-  .progress-bar .progress {
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 20px;
-  }
-
-  .progress-bar .progress div {
-    display: inline-block;
-    margin: 1px;
-    height: 16px;
-    width: 10px;
-    background: #072269;
-  }
-}
-</style>

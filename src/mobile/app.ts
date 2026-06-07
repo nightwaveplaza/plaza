3ffedc1b48c29ce2bb62ecb1ac7b1ebe1815c4fd
Alexander <9192541+potatoru@users.noreply.github.com>
2026-06-07 23:34:36 +1100

    Fix capcha on mobile

--- src/mobile/app.ts
+++ src/mobile/app.ts
@@ -2,6 +2,7 @@ import '@mobile/styles/dist.scss';
 import '@app/styles/ui/ui.scss';
 import '@mobile/styles/app.scss';
 
+import 'altcha';
 import { createApp, h } from 'vue';
 import plazaComponents from '@app/components/basic';
 import plazaExtraComponents from '@app/components/extra';
@@ -9,6 +10,13 @@ import plazaPlayer from '@app/components/player';
 import plazaWindows from '@app/components/windows';
 import App from '@mobile/App.vue';
 import { i18n } from '@locales/_i18n.ts';
+
+$altcha.defaults.set({
+  challenge: 'https://api.plaza.one/altcha-challenge',
+  debug: true,
+  hideLogo: true,
+  auto: 'onload',
+});
 
 const app = createApp({
   render: () => h(App),
